9e5e0b8359899d56963a2a1a6c32676a5a047365
Ted G <edwardjohng@gmail.com>
2018-04-14 13:44:02 -0700

    Finally added stuff

---
+++ imu.ino
@@ -0,0 +1,133 @@
+
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+#endif
+
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+#define LED_PIN 13
+//#include "MPU6050.h" // not necessary if using MotionApps include file
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+
+
+Quaternion mpuQ; //This is the quaternion we'll get from the IMU
+MPU6050 mpu;
+
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+int dmpStatus;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+void setup()
+{
+    pinMode(LED_PIN, OUTPUT);
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+    
+    mpu.initialize();
+    pinMode(2, INPUT); //Interrupt pin
+    dmpStatus = mpu.dmpInitialize();
+    //Gyro Offsets????
+
+    //These were all grabbed using the ZEROing example code provided in the mpu6050 library
+    mpu.setXAccelOffset(-375);
+    mpu.setYAccelOffset(-4783);
+    mpu.setZAccelOffset(1445);
+    mpu.setXGyroOffset(51);
+    mpu.setYGyroOffset(-12);
+    mpu.setZGyroOffset(-3);
+    
+
+    if(dmpStatus == 0)
+        mpu.setDMPEnabled(true);
+
+    packetSize = mpu.dmpGetFIFOPacketSize();
+
+   // attachInterrupt(digitalPinToInterrupt(2), dmpDataReady, RISING);
+   Serial.begin(57600);
+}
+
+void loop()
+{
+   // digitalWrite(LED_PIN,LOW);
+
+    if(dmpStatus != 0)
+        return;
+    mpuInterrupt = false;
+
+    mpuIntStatus = mpu.getIntStatus();
+    fifoCount = mpu.getFIFOCount();
+
+     if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        mpu.resetFIFO();
+     }
+    else if(mpuIntStatus & 0x02)
+    {
+       
+         // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+        //digitalWrite(LED_PIN, HIGH);
+         // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+        mpu.dmpGetQuaternion(&mpuQ, fifoBuffer);
+        mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+        //begin sending over serial
+        Serial.println("a");//This just makes it so we can sync by calling readline.
+        //Start with the quaternion
+        byte * sendingReference = (byte*) &mpuQ.w;
+        Serial.write(sendingReference,4);
+        sendingReference = (byte*) &mpuQ.x;
+        Serial.write(sendingReference,4);
+        sendingReference = (byte*) &mpuQ.y;
+        Serial.write(sendingReference,4);
+        sendingReference = (byte*) &mpuQ.z;
+        Serial.write(sendingReference,4);
+        
+        //start sending accelerometer data.
+        sendingReference = (byte*) &ax;
+        Serial.write(sendingReference,2);   
+        sendingReference = (byte*) &ay;
+        Serial.write(sendingReference,2);
+        sendingReference = (byte*) &az;
+        Serial.write(sendingReference,2);      
+
+        //start sending gyro data.
+        sendingReference = (byte*) &gx;
+        Serial.write(sendingReference,2);   
+        sendingReference = (byte*) &gy;
+        Serial.write(sendingReference,2);
+        sendingReference = (byte*) &gz;
+        Serial.write(sendingReference,2);      
+        delay(10);
+
+    }
+    
+}
---
+++ scripts/imu.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import math
+import serial
+import struct
+import rospy
+from sensor_msgs.msg import Imu
+
+# -32768 to 32767 for a 16 bit int
+# The next two lines specify the default ranges for the MPU 6050.
+
+# print(ser.name)         # check which port was really used
+
+def imuSender():
+    aRange = 2  # +/- g
+    gRange = 250  # +/- degrees/second
+    GACCEL = 9.80655  # m/s^2
+    DEG_TO_RAD = math.pi / 180
+    ser = serial.Serial('/dev/ttyUSB0', 57600)  # open serial port
+    pub = rospy.Publisher('imu_backup', Imu, queue_size=10)
+    rospy.init_node('imub')
+    rate = rospy.Rate(100)  # 100Hz
+    msg = Imu()
+    msg.header.frame_id = 1
+    inRaw = 0
+    print('begin')
+    while not rospy.is_shutdown():
+        ser.readline()
+        # next comes the quaternions
+        floats = [0.0, 0.0, 0.0, 0.0]  # wxyz
+        for x in range(0, 4):
+            inRaw = ser.read(4)
+            floats[x] = struct.unpack('f', inRaw)[0]
+
+        msg.orientation.w = floats[0]
+        msg.orientation.x = floats[1]
+        msg.orientation.y = floats[2]
+        msg.orientation.z = floats[3]
+
+        # accelerations are next
+        accels = [0.0, 0.0, 0.0]
+        for x in range(0, 3):
+            inRaw = ser.read(2);  # read two bytes
+            accels[x] = struct.unpack('h', inRaw)[0]
+            if accels[x] > 0:
+                accels[x] = (accels[x] / (32767 / aRange)) * GACCEL
+            else:
+                accels[x] = (accels[x] / (32768 / aRange)) * GACCEL
+
+        msg.linear_acceleration.x = accels[0]
+        msg.linear_acceleration.y = accels[1]
+        msg.linear_acceleration.z = accels[2]
+
+        # next comes gyro data
+        accels = [0.0, 0.0, 0.0]
+        for x in range(0, 3):
+            inRaw = ser.read(2);
+            accels[x] = struct.unpack('h', inRaw)[0]
+            if accels[x] > 0:
+                accels[x] = (accels[x] / (32767 / gRange)) * DEG_TO_RAD
+            else:
+                accels[x] = (accels[x] / (32768 / gRange)) * DEG_TO_RAD
+
+        msg.angular_velocity.x = accels[0]
+        msg.angular_velocity.y = accels[1]
+        msg.angular_velocity.y = accels[2]
+
+        pub.publish(msg)
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        imuSender()
+    except rospy.ROSInterruptException:
+        pass
